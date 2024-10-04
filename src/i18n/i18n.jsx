82a1c77417c1c23db185d57558e9aaad29ddf03f
ReaksmeyThan <reaksmey.than7up@gmail.com>
2024-10-04 12:44:27 +0700

    update

--- src/i18n/i18n.jsx
+++ src/i18n/i18n.jsx
@@ -16,7 +16,7 @@ i18n
     },
     fallbackLng: 'en',
     // disabled in production
-    debug: false,
+    debug: true,
     // can have multiple namespaces, in case you want to divide a huge
     // translation into smaller pieces and load them on demand
     ns: ['tran'],
