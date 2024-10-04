0a205d29d3db4dd2c5803b52d52151a9cc73f2a3
ReaksmeyThan <reaksmey.than7up@gmail.com>
2024-10-04 12:42:32 +0700

    update load path

--- src/i18n/i18n.jsx
+++ src/i18n/i18n.jsx
@@ -10,7 +10,9 @@ i18n
   .init({
     backend: {
       // translation file path
-      loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
+      // loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
+      loadPath: `${process.env.PUBLIC_URL}/assets/i18n/{{ns}}/{{lng}}.json`,
+
     },
     fallbackLng: 'en',
     // disabled in production
