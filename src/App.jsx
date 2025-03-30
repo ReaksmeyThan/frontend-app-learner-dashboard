dd5899877a6d6c08224ccfdfe63b13c009361bfa
ReaksmeyThan <reaksmey.than7up@gmail.com>
2025-03-30 16:19:27 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
-import { initializeHotjar } from "@edx/frontend-enterprise-hotjar";
-import { useIntl } from "@edx/frontend-platform/i18n";
-import { logError } from "@edx/frontend-platform/logging";
+import { initializeHotjar } from '@edx/frontend-enterprise-hotjar';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import { logError } from '@edx/frontend-platform/logging';
 
-import { AppContext, ErrorPage } from "@edx/frontend-platform/react";
-import { Alert } from "@openedx/paragon";
+import { AppContext, ErrorPage } from '@edx/frontend-platform/react';
+import { Alert } from '@openedx/paragon';
 
-import ZendeskFab from "components/ZendeskFab";
-import Dashboard from "containers/Dashboard";
-import { RequestKeys } from "data/constants/requests";
-import { actions, selectors } from "data/redux";
-import store from "data/store";
-import { reduxHooks } from "hooks";
+import ZendeskFab from 'components/ZendeskFab';
+import Dashboard from 'containers/Dashboard';
+import { RequestKeys } from 'data/constants/requests';
+import { actions, selectors } from 'data/redux';
+import store from 'data/store';
+import { reduxHooks } from 'hooks';
 
-import track from "tracking";
+import track from 'tracking';
 
-import fakeData from "data/services/lms/fakeData/courses";
+import fakeData from 'data/services/lms/fakeData/courses';
 
-import LearnerDashboardHeader from "containers/LearnerDashboardHeader";
-import AppWrapper from "containers/WidgetContainers/AppWrapper";
+import LearnerDashboardHeader from 'containers/LearnerDashboardHeader';
+import AppWrapper from 'containers/WidgetContainers/AppWrapper';
 
-import { getConfig } from "@edx/frontend-platform";
+import { getConfig } from '@edx/frontend-platform';
 import MyFooter from 'components/Footer'; // Adjust the path if necessary
-import "./App.scss";
-import messages from "./messages";
-
+import i18next from 'i18next';
+import './App.scss';
+import messages from './messages';
 
 export const App = () => {
   const { authenticatedUser } = React.useContext(AppContext);
@@ -40,7 +40,7 @@ export const App = () => {
   const loadData = reduxHooks.useLoadData();
 
   React.useEffect(() => {
-    if (authenticatedUser?.administrator || getConfig().NODE_ENV === "development") {
+    if (authenticatedUser?.administrator || getConfig().NODE_ENV === 'development') {
       window.loadEmptyData = () => {
         loadData({ ...fakeData.globalData, courses: [] });
       };
@@ -90,7 +90,7 @@ export const App = () => {
             )}
           </main>
         </AppWrapper>
-        <MyFooter />
+        <MyFooter onLanguageSelected={(language) => { i18next.changeLanguage(language); }} />
         <ZendeskFab />
       </div>
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,37 @@
-import Footer from "@edx/frontend-component-footer-edx";
-import React from "react";
+import { IntlProvider } from '@edx/frontend-platform/i18n';
+import { AppContext } from '@edx/frontend-platform/react';
+import PropTypes from 'prop-types';
+import React, { useMemo } from 'react';
 
-import "./Footer.scss";
+import Footer from '@edx/frontend-component-footer-edx';
 
-const MyFooter = () => {
+const MyFooter = ({ onLanguageSelected = () => {} }) => {
+  const contextValue = useMemo(
+    () => ({
+      authenticatedUser: null,
+      config: {
+        LOGO_TRADEMARK_URL: process.env.LOGO_TRADEMARK_URL,
+      },
+    }),
+    [],
+  );
+
   return (
-    <Footer
-      onLanguageSelected={(languageCode) => {
-        /* set language */
-      }}
-      supportedLanguages={[
-        { label: "English", value: "en" },
-        { label: "Español", value: "es" },
-      ]}
-    />
+    <IntlProvider locale={contextValue.config.LOCALE}>
+      <AppContext.Provider value={contextValue}>
+        <Footer
+          onLanguageSelected={onLanguageSelected}
+          supportedLanguages={[
+            { label: 'English', value: 'en' },
+            { label: 'ខ្មែរ', value: 'km-kh' },
+          ]}
+        />
+      </AppContext.Provider>
+    </IntlProvider>
   );
 };
+MyFooter.propTypes = {
+  onLanguageSelected: PropTypes.func,
+};
+
 export default MyFooter;
