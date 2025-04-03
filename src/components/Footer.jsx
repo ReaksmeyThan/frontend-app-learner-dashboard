7d784e2d3cd7eb1b220d89d91a21846857c47e41
ReaksmeyThan <reaksmey.than7up@gmail.com>
2025-04-04 01:13:11 +0700

    close select lang footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
-import Footer from '@edx/frontend-component-footer-edx';
 import { getConfig } from '@edx/frontend-platform';
-import { IntlProvider } from '@edx/frontend-platform/i18n';
-import { AppContext } from '@edx/frontend-platform/react';
+import i18next from 'i18next';
 import PropTypes from 'prop-types';
-import React, { useMemo } from 'react';
+import React, { useState } from 'react';
+
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import logoWhite from '../assets/logo-white.png';
@@ -11,86 +10,91 @@ import './Footer.scss';
 
 const MyFooter = ({ onLanguageSelected = () => {} }) => {
   const { t } = useTranslation();
-  const contextValue = useMemo(
-    () => ({
-      authenticatedUser: null,
-      config: {
-        LOGO_TRADEMARK_URL: process.env.LOGO_TRADEMARK_URL,
-      },
-    }),
-    [],
-  );
+
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+
+  const handleLanguageChange = (event) => {
+    const language = event.target.value;
+    setSelectedLanguage(language);
+    i18next.changeLanguage(language).catch((error) => {
+      console.error('Error changing language:', error);
+    });
+  };
 
   return (
-    <IntlProvider locale={contextValue.config.LOCALE}>
-      <AppContext.Provider value={contextValue}>
-        <div className="d-flex row align-content-center justify-content-center">
-          <footer className="footer-container">
-            {/* Bottom Footer */}
-            <div className="footer-bottom font-red-hat-display">
-              <Container fluid>
-                <Row className="text-left text-white text-md-left">
-                  {/* Logo */}
-                  <Col xs={12} md={12} lg={2} className="pl-0 d-flex justify-content-center">
-                    <img src={logoWhite} alt="Logo" className="logo" />
-                  </Col>
+    <div className="d-flex row align-content-center justify-content-center">
+      <footer className="footer-container">
+        {/* Bottom Footer */}
+        <div className="footer-bottom font-red-hat-display">
+          <Container fluid>
+            <Row className="text-left text-white text-md-left">
+              {/* Logo */}
+              <Col xs={12} md={12} lg={2} className="pl-0 d-flex justify-content-center">
+                <img src={logoWhite} alt="Logo" className="logo" />
+              </Col>
 
-                  {/* Copyright */}
-                  <Col xs={12} md={12} lg={3} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
-                    <p className="small m-0">&copy; 2024 Credit Bureau Cambodia. All rights reserved.</p>
-                  </Col>
+              {/* Copyright */}
+              <Col xs={12} md={12} lg={3} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+                <p className="small m-0">&copy; 2024 Credit Bureau Cambodia. All rights reserved.</p>
+              </Col>
 
-                  {/* Links */}
-                  <Col xs={12} md={12} lg={4} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
-                    <ul className="list-inline m-0">
-                      <li className="list-inline-item mx-2">
-                        <a href={getConfig().LMS_BASE_URL} className="text-white small" data-translate-key="myCourse">
-                          {t('myCourse')}
+              {/* Links */}
+              <Col xs={12} md={12} lg={4} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+                <ul className="list-inline m-0">
+                  <li className="list-inline-item mx-2">
+                    <a href={getConfig().LMS_BASE_URL} className="text-white small" data-translate-key="myCourse">
+                      {t('myCourse')}
+                    </a>
+                  </li>
+                  |
+                  <li className="list-inline-item mx-2">
+                    <a href={`${getConfig().LMS_BASE_URL}/library`} className="text-white small" data-translate-key="library">
+                      {t('library')}
+                    </a>
+                  </li>
+                  |
+                  <li className="list-inline-item mx-2">
+                    <a href={`${getConfig().LMS_BASE_URL}/contact`} className="text-white small" data-translate-key="contactUs">
+                      {t('contactUs')}
+                    </a>
+                  </li>
+                  |
+                  <li className="list-inline-item mx-2">
+                    <a href={getConfig().ACCOUNT_SETTINGS_URL} className="text-white small" data-translate-key="account">
+                      {t('account')}
+                    </a>
+                  </li>
+                </ul>
+              </Col>
 
-                        </a>
-                      </li>
-                      |
-                      <li className="list-inline-item mx-2">
-                        <a href={`${getConfig().LMS_BASE_URL}/library`} className="text-white small" data-translate-key="library">
+              {/* Language Selector */}
+              {/* <Col xs={9} md={9} lg={3} className="pb-4">
+                <label htmlFor="language-selector" className="text-white small">
+                  {t('selectLanguage')}
+                </label>
+                <select
+                  id="language-selector"
+                  className="form-select mt-3"
+                  value={selectedLanguage}
+                  onChange={setSelectedLanguage}
 
-                          {t('library')}
-
-                        </a>
-                      </li>
-                      |
-                      <li className="list-inline-item mx-2">
-                        <a href={`${getConfig().LMS_BASE_URL}/contact`} className="text-white small" data-translate-key="contactUs">
-                          {t('contactUs')}
-
-                        </a>
-                      </li>
-                      |
-                      <li className="list-inline-item mx-2">
-                        <a href={getConfig().ACCOUNT_SETTINGS_URL} className="text-white small" data-translate-key="account">
-                          {t('account')}
-
-                        </a>
-                      </li>
-                    </ul>
-                  </Col>
-
-                  {/* Language Selector */}
-                  <Col xs={9} md={9} lg={3} className="pb-4">
-                    <Footer
-                      onLanguageSelected={onLanguageSelected}
-                      supportedLanguages={[
-                        { label: 'English', value: 'en' },
-                        { label: "ខ្មែរ", value: "km-kh" }, // eslint-disable-line
-                      ]}
-                    />
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-          </footer>
+                >
+                  <option value="en">English</option>
+                  <option value="km-kh">{t('khmer')}</option>
+                </select> */}
+                {/* <button
+                  type="button"
+                  className="btn btn-primary mt-3"
+                  onClick={() => handleLanguageChange({ target: { value: selectedLanguage } })}
+                >
+                  Submit
+                </button> */}
+              {/* </Col> */}
+            </Row>
+          </Container>
         </div>
-      </AppContext.Provider>
-    </IntlProvider>
+      </footer>
+    </div>
   );
 };
 MyFooter.propTypes = {
