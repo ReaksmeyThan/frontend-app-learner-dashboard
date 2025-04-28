3d299d47e17322172d2f933fa23f9807132879c5
Kanharith Sok <kanharithsok@gmail.com>
2025-04-29 00:15:59 +0700

    Fixed header translations

--- src/containers/LearnerDashboardHeader/ExpandedHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-// import { useIntl } from '@edx/frontend-platform/i18n';
+import { useIntl } from '@edx/frontend-platform/i18n';
 import { Button } from '@openedx/paragon';
 
 import { getConfig } from '@edx/frontend-platform';
 import urls from 'data/services/lms/urls';
 import { reduxHooks } from 'hooks';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import BrandLogo from '../BrandLogo';
 import { findCoursesNavClicked, useIsCollapsed } from '../hooks';
 import AuthenticatedUserDropdown from './AuthenticatedUserDropdown';
 
 export const ExpandedHeader = () => {
-  const { t } = useTranslation(); // Get the t function from useTranslation
-  // const { formatMessage } = useIntl();
+  // const { t } = useTranslation(); // Get the t function from useTranslation
+  const { formatMessage } = useIntl();
   const { courseSearchUrl } = reduxHooks.usePlatformSettingsData();
   const isCollapsed = useIsCollapsed();
 
@@ -29,16 +29,16 @@ export const ExpandedHeader = () => {
         <div className=" flex-grow-1 d-flex align-items-center w-100">
           <BrandLogo />
           <Button as="a" href={`${getConfig().LMS_BASE_URL}`} variant="inverse-primary" className="p-4">
-            {t('home')}
+          {formatMessage(messages.home)}
           </Button>
           <Button as="a" href="urls.baseAppUrl(courseSearchUrl)" variant="inverse-primary" className="p-4 course-link">
 
-            {t('course')}
+          {formatMessage(messages.course)}
           </Button>
 
           <Button as="a" href={urls.baseAppUrl(courseSearchUrl)} variant="inverse-primary" className="p-4" onClick={exploreCoursesClick}>
 
-            {t('discoverNew')}
+          {formatMessage(messages.discoverNew)}
           </Button>
 
           <span className="flex-grow-1" />
